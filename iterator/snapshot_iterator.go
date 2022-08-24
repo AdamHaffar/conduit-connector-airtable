bee325ba0608e8fd54d783bf36969983c1e47047
AdamHaffar <haffaradam@yahoo.com>
2022-08-24 19:35:15 +0100

    remove position.go

--- iterator/snapshot_iterator.go
+++ iterator/snapshot_iterator.go
@@ -8,9 +8,9 @@ import (
 )
 
 type SnapshotIterator struct {
-	client   *airtableclient.Client
-	data     *airtableclient.Records
-	position int
+	client      *airtableclient.Client
+	data        *airtableclient.Records
+	internalPos int
 }
 
 func NewSnapshotIterator(client airtableclient.Client, config config.Config, pos int) (*SnapshotIterator, error) {
@@ -26,25 +26,66 @@ func NewSnapshotIterator(client airtableclient.Client, config config.Config, pos
 	//records successful
 
 	s := &SnapshotIterator{
-		client:   &client,
-		data:     records,
-		position: pos,
+		client:      &client,
+		data:        records,
+		internalPos: 0,
 	}
 	return s, nil
 }
 
-func (w *SnapshotIterator) HasNext(ctx context.Context) bool {
-	if w.position == len(w.data.Records) {
+func (s *SnapshotIterator) HasNext(ctx context.Context) bool {
+	if s.internalPos == len(s.data.Records)+1 {
 		return false
 	}
 	return true
 }
 
-func (w *SnapshotIterator) Next(ctx context.Context) (sdk.Record, error) {
-	return nil
+func (s *SnapshotIterator) Next(ctx context.Context) (sdk.Record, error) {
+
+	if err := ctx.Err(); err != nil {
+		return sdk.Record{}, err
+	}
+
+	s.internalPos++ // increment internal position
+	rec := sdk.Util.Source.NewRecordSnapshot(
+		s.buildRecordPosition(),
+		s.buildRecordMetadata(),
+		s.buildRecordKey(),
+		s.buildRecordPayload(),
+	)
+
+	return rec, nil
 }
-func (w *SnapshotIterator) Stop() {
+func (s *SnapshotIterator) Stop() {
 }
 
-func (s *SnapshotIterator) prepareRecord(ctx context.Context, data []string) (sdk.Record, error) {
+func (s *SnapshotIterator) buildRecordPosition() sdk.Position {
+	position := s.data.Records[s.internalPos].ID //ID of individual record
+	return sdk.Position(position)
+}
+
+func (s *SnapshotIterator) buildRecordMetadata() map[string]string {
+	return map[string]string{
+		"DatabaseID": config.BaseID,
+		"TableID":    config.TableID,
+	}
+}
+
+// buildRecordKey returns the key for the record.
+func (s *SnapshotIterator) buildRecordKey(values []interface{}) sdk.Data {
+	if s.keyColumnIndex == -1 {
+		return nil
+	}
+	return sdk.StructuredData{
+		// TODO handle composite keys
+		s.config.KeyColumn: values[s.keyColumnIndex],
+	}
+}
+
+func (s *SnapshotIterator) buildRecordPayload(values []interface{}) sdk.Data {
+	payload := make(sdk.StructuredData)
+	for i, val := range values {
+		payload[s.config.Columns[i]] = val
+	}
+	return payload
 }
